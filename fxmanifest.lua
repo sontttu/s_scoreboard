1a5d77dcdb13864153366cafb8fe6b5baf641e6b
SONTTU <bytex87@gmail.com>
2025-05-18 15:01:41 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'sonttu'
+description 'Scoreboard'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/images/logo.png'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
--- server/server.lua
+++ server/server.lua
@@ -1,18 +1,41 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local Framework = nil
+local isQBCore = false
+
+if GetResourceState('qb-core') == 'started' then
+    Framework = exports['qb-core']:GetCoreObject()
+    isQBCore = true
+elseif GetResourceState('es_extended') == 'started' then
+    Framework = exports['es_extended']:getSharedObject()
+end
 
 RegisterNetEvent('scoreboard:getPlayers', function()
     local src = source
-    local players = QBCore.Functions.GetPlayers()
     local playerData = {}
 
-    for _, playerId in pairs(players) do
-        local Player = QBCore.Functions.GetPlayer(playerId)
-        if Player then
-            table.insert(playerData, {
-                id = playerId,
-                name = Player.PlayerData.charinfo.firstname .. ' ' .. Player.PlayerData.charinfo.lastname,
-                job = Player.PlayerData.job.name
-            })
+    if isQBCore then
+        local players = Framework.Functions.GetPlayers()
+        for _, playerId in pairs(players) do
+            local Player = Framework.Functions.GetPlayer(playerId)
+            if Player then
+                table.insert(playerData, {
+                    id = playerId,
+                    name = Player.PlayerData.charinfo.firstname .. ' ' .. Player.PlayerData.charinfo.lastname,
+                    job = Player.PlayerData.job.name
+                })
+            end
+        end
+    else
+        for _, playerId in ipairs(GetPlayers()) do
+            local xPlayer = Framework.GetPlayerFromId(tonumber(playerId))
+            if xPlayer then
+                local name = GetPlayerName(playerId)
+                local job = xPlayer.job and xPlayer.job.name or "unknown"
+                table.insert(playerData, {
+                    id = tonumber(playerId),
+                    name = name,
+                    job = job
+                })
+            end
         end
     end
 
